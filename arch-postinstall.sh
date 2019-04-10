eede5aa842732614ad388564972a3cc58d4b13cf
nipsufn <ferdynand.naczynski@gmail.com>
2019-04-10 14:13:59 +0200

    fix permissions

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -11,22 +11,21 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf
 sed -i 's/#\[multilib\]/[multilib]/g' /etc/pacman.conf
 sed -i '/^\[multilib\]$/,+1 s.^#..' /etc/pacman.conf
-useradd -s /bin/zsh $USER
+useradd -m -k -s /bin/zsh $USER
 usermod -aG wheel,users,games,uucp,audio,disk,floppy,input,kvm,optical,scanner,storage,video,network,power $USER
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
 
-su $USER
-cd
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -s /usr/share/zsh-theme-powerlevel9k /$USER/home/.oh-my-zsh/custom/themes/powerlevel9k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k/powerlevel9k"/g' /home/$USER/.zshrc
-sed -i '1iexport TERM="xterm-256color"' /home/$USER/.zshrc
-sed -i '1iexport LANG="en_US.UTF-8"' /home/$USER/.zshrc
-sed -i '1iif [ "$TERM" = "linux" ]; then source .zshrc_nox; exit; fi' /home/$USER/.zshrc
+cd /home/$USER
+sudo -u $USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo -u $USER ln -s /usr/share/zsh-theme-powerlevel9k /$USER/home/.oh-my-zsh/custom/themes/powerlevel9k
+sudo -u $USER sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k/powerlevel9k"/g' /home/$USER/.zshrc
+sudo -u $USER sed -i '1iexport TERM="xterm-256color"' /home/$USER/.zshrc
+sudo -u $USER sed -i '1iexport LANG="en_US.UTF-8"' /home/$USER/.zshrc
+sudo -u $USER sed -i '1iif [ "$TERM" = "linux" ]; then source .zshrc_nox; exit; fi' /home/$USER/.zshrc
 
-cp /etc/skel/.zshrc ./zshrc_nox
+sudo -u $USER cp /etc/skel/.zshrc ./zshrc_nox
 
-wget https://raw.githubusercontent.com/nipsufn/arch-install/master/.screenrc
-wget https://raw.githubusercontent.com/nipsufn/arch-install/master/.startup.sh
+sudo -u $USER wget https://raw.githubusercontent.com/nipsufn/arch-install/master/.screenrc
+sudo -u $USER wget https://raw.githubusercontent.com/nipsufn/arch-install/master/.startup.sh
 
-printf "\n~/.startup.sh\n" >> .zshrc_nox
+sudo -u $USER printf "\n~/.startup.sh\n" >> .zshrc_nox
