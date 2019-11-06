cb4606cc31f00768af2ca2252d2b5a7036143621
nipsufn <ferdynand.naczynski@gmail.com>
2019-11-06 19:20:46 +0100

    base package change

--- arch-install.sh
+++ arch-install.sh
@@ -5,7 +5,7 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf
 sed -i 's/#\[multilib\]/[multilib]/g' /etc/pacman.conf
 sed -i '/^\[multilib\]$/,+1 s.^#..' /etc/pacman.conf
-pacstrap /mnt base base base-devel grub xorg xorg-apps xorg-xinit xdotool xbindkeys cinnamon networkmanager steam code firefox telegram-desktop syncthing-gtk keepassxc atril eom gimp blender vlc clementine mate-terminal tilda nemo-fileroller nemo-preview nemo-share gvfs-mtp gvfs-gphoto2 gvfs-afc gvfs-smb ntfs-3g intel-ucode cpupower alsa-utils alsa-oss zsh zsh-completions zsh-theme-powerlevel9k powerline-fonts screen tree git openssh ttf-dejavu noto-fonts ttf-liberation ttf-freefont
+pacstrap /mnt base linux linux-firmware e2fsprogs exfat-utils dosfstools ntfs-3g vim man-db man-pages textinfo base-devel grub intel-ucode cpupower alsa-utils alsa-oss screen tree git openssh xorg xorg-apps xorg-xinit xdotool xbindkeys cinnamon networkmanager steam code firefox telegram-desktop keepassxc atril eom gimp blender vlc clementine mate-terminal tilda nemo-fileroller nemo-preview nemo-share gvfs-mtp gvfs-gphoto2 gvfs-afc gvfs-smb ttf-dejavu noto-fonts ttf-liberation ttf-freefont
 #  nvidia nvidia-utils lib32-nvidia-utils
 #  xf86-video-ati mesa lib32-mesa xf86-input-libinput
 genfstab -U /mnt >> /mnt/etc/fstab
